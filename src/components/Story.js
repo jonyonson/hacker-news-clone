699ee981d1d4e6fa9b42b36d6fe6b3598497c350
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-09-28 20:29:29 -0500

    convert timestamp to relative time

--- src/components/Story.js
+++ src/components/Story.js
@@ -6,6 +6,7 @@ import {
   StoryMeta,
   StoryMetaElement,
 } from '../styles/StoryStyles';
+import { mapTime } from '../mappers/mapTime';
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -25,7 +26,7 @@ export const Story = ({ storyId }) => {
         </span>
         <span className="story__time" data-testid="story-time">
           <StoryMetaElement color="#000000">Posted:</StoryMetaElement>{' '}
-          {story.time}
+          {mapTime(story.time)} ago
         </span>
       </StoryMeta>
     </StoryWrapper>
