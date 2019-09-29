26871252812a194a8f072c4e9133cd06ad00dbb1
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-09-28 20:03:29 -0500

    pulling stories

---
+++ src/components/Story.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { getStory } from '../services/hnApi';
+import {
+  StoryWrapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement,
+} from '../styles/StoryStyles';
+
+export const Story = ({ storyId }) => {
+  const [story, setStory] = useState({});
+
+  useEffect(() => {
+    getStory(storyId).then((data) => data && data.url && setStory(data));
+  }, [storyId]);
+
+  return story && story.url ? (
+    <StoryWrapper data-testid="story">
+      <StoryTitle>
+        <a href={story.url}>{story.title}</a>
+      </StoryTitle>
+      <StoryMeta>
+        <span className="story__by" data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
+        </span>
+        <span className="story__time" data-testid="story-time">
+          <StoryMetaElement color="#000000">Posted:</StoryMetaElement>{' '}
+          {story.time}
+        </span>
+      </StoryMeta>
+    </StoryWrapper>
+  ) : null;
+};
---
+++ src/containers/StoriesContainer.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { getStoryIds } from '../services/hnApi';
+import { Story } from '../components/Story';
+import {
+  GlobalStyle,
+  StoriesContainerWrapper,
+} from '../styles/StoriesContainerStyles';
+
+export const StoriesContainer = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then((data) => setStoryIds(data));
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories=container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map((storyId) => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
+    </>
+  );
+};
